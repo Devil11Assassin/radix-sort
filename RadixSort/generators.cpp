a51a7d6af66a5cfaa02a52a55a5aa9f4d8f7c444
Devil Assassin <83004869+Devil11Assassin@users.noreply.github.com>
2026-01-06 09:44:43 +0200

    Updated generators.hpp/cpp to better templated code.

--- RadixSort/generators.cpp
+++ RadixSort/generators.cpp
@@ -1,148 +1,20 @@
 #include "generators.hpp"
-#include <random>
-#include <limits>
-#include <bit>
+
 using namespace std;
 
-vector<int> generators::generateInts(int n)
-{
-	vector<int> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	//uniform_int_distribution<int> dist(0, INT_MAX);
-	uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
-
-	while (n--)
-		v.emplace_back(dist(gen));
-
-	return v;
-}
-
-vector<unsigned int> generators::generateUnsignedInts(int n)
-{
-	vector<unsigned int> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	uniform_int_distribution<int> dist(0, UINT_MAX);
-
-	while (n--)
-		v.emplace_back(dist(gen));
-
-	return v;
-}
-
-vector<long long> generators::generateLLs(int n)
-{
-	vector<long long> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	uniform_int_distribution<long long> dist(LLONG_MIN, LLONG_MAX);
-
-	while (n--)
-		v.emplace_back(dist(gen));
-
-	return v;
-}
-
-vector<unsigned long long> generators::generateULLs(int n)
-{
-	vector<unsigned long long> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	uniform_int_distribution<unsigned long long> dist(0, ULLONG_MAX);
-
-	while (n--)
-		v.emplace_back(dist(gen));
-
-	return v;
-}
-
-vector<float> generators::generateFloats(int n) 
-{
-	vector<float> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	uniform_int_distribution<unsigned int> dist(0, UINT_MAX);
-
-	while (n--)
-	{
-		unsigned int num = 0;
-		while (true) 
-		{
-			num = dist(gen);
-
-			if ((num & 0x7F800000) != 0x7F800000) 
-				break;
-		}
-		v.emplace_back(bit_cast<float>(num));
-	}
-
-	return v;
-}
-
-vector<double> generators::generateDoubles(int n) 
-{
-	vector<double> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
-	uniform_int_distribution<unsigned long long> dist(0, ULLONG_MAX);
-
-	while (n--)
-	{
-		unsigned long long num = 0;
-		while (true) 
-		{
-			num = dist(gen);
-			if ((num & 0x7F80000000000000) != 0x7F80000000000000) break;
-		}
-		v.emplace_back(bit_cast<double>(num));
-	}
-
-	return v;
-}
-
-vector<string> generators::generateStringsFixed(int n, int len)
+vector<string> generators::generateStrings(int n, int maxLen, const bool fixed)
 {
 	vector<string> v;
 	v.reserve(n);
 
-	mt19937 gen(69);
-	//uniform_int_distribution<int> charDist(32, 126);
-	uniform_int_distribution<int> charDist(0, 255);
-
-	while (n--)
-	{
-		string s;
-		s.resize(len);
-
-		for (int i = 0; i < len; i++)
-			s[i] = static_cast<char>(charDist(gen));
-
-		v.emplace_back(move(s));
-	}
-
-	return v;
-}
-
-vector<string> generators::generateStrings(int n, int maxLen)
-{
-	vector<string> v;
-	v.reserve(n);
-
-	mt19937 gen(69);
+	mt19937_64 gen(69);
 	uniform_int_distribution<int> lenDist(0, maxLen);
-	//uniform_int_distribution<int> charDist(32, 126);
-	uniform_int_distribution<int> charDist(0, 255);
+	uniform_int_distribution<int> charDist(0, 255); // or (32, 126)
 
 	while (n--)
 	{
-		int len = lenDist(gen);
+		int len = (fixed)? maxLen : lenDist(gen);
+		
 		string s;
 		s.resize(len);
 
--- RadixSort/generators.hpp
+++ RadixSort/generators.hpp
@@ -1,48 +1,80 @@
 #pragma once
-#include <vector>
+#include <bit>
+#include <concepts>
+#include <cstdint>
+#include <limits>
+#include <random>
 #include <string>
-
-static_assert(sizeof(int) == 4,
-	"ERROR: generators.hpp requires sizeof(int) == 4 bytes.\n");
-
-static_assert(sizeof(long long) == 8,
-	"ERROR: generators.hpp requires sizeof(long long) == 8 bytes.\n");
-
-static_assert(sizeof(float) == 4 && std::numeric_limits<float>::is_iec559,
-	"ERROR: generators.hpp requires IEEE-754 binary32 float (4 bytes).\n");
-
-static_assert(sizeof(double) == 8 && std::numeric_limits<double>::is_iec559,
-	"ERROR: generators.hpp requires IEEE-754 binary64 double (8 bytes).\n");
+#include <vector>
 
 class generators
 {
+	static constexpr int SEED = 69;
+
+	template <size_t S> struct fp2i_imp;
+	template <> struct fp2i_imp<2> { using type = std::uint16_t; static constexpr type mask = 0x7C00; };
+	template <> struct fp2i_imp<4> { using type = std::uint32_t; static constexpr type mask = 0x7F800000; };
+	template <> struct fp2i_imp<8> { using type = std::uint64_t; static constexpr type mask = 0x7FF0000000000000; };
+
+	template <typename T>
+	using fp2i = fp2i_imp<sizeof(T)>;
+
+	static std::vector<std::string> generateStrings(int n, int maxLen = 20, const bool fixed = false);
+
 public:
-	static std::vector<int> generateInts(int n);
-	static std::vector<unsigned int> generateUnsignedInts(int n);
-	static std::vector<long long> generateLLs(int n);
-	static std::vector<unsigned long long> generateULLs(int n);
-	static std::vector<float> generateFloats(int n);
-	static std::vector<double> generateDoubles(int n);
-	static std::vector<std::string> generateStringsFixed(int n, int len);
-	static std::vector<std::string> generateStrings(int n, int maxLen);
-	
-	template<typename T>
+	template <std::integral T>
 	static std::vector<T> generate(int n)
 	{
-		if constexpr (std::is_same_v<T, int>)
-			return generateInts(n);
-		else if constexpr (std::is_same_v<T, unsigned int>)
-			return generateUnsignedInts(n);
-		else if constexpr (std::is_same_v<T, long long>)
-			return generateLLs(n);
-		else if constexpr (std::is_same_v<T, unsigned long long>)
-			return generateULLs(n);
-		else if constexpr (std::is_same_v<T, float>)
-			return generateFloats(n);
-		else if constexpr (std::is_same_v<T, double>)
-			return generateDoubles(n);
-		else if constexpr (std::is_same_v<T, std::string>)
-			return generateStrings(n, 20);
+		std::vector<T> v;
+		v.reserve(n);
+
+		std::mt19937_64 gen(SEED);
+		std::uniform_int_distribution<T> dist(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+
+		while (n--)
+			v.emplace_back(dist(gen));
+
+		return v;
+	}
+
+	template <std::floating_point T>
+	static std::vector<T> generate(int n, bool testStrongOrder = true)
+	{
+		static_assert(
+			std::numeric_limits<T>::is_iec559,
+			"ERROR: generators.hpp requires IEEE-754/IEC-559 compliance.\n"
+		);
+
+		std::vector<T> v;
+		v.reserve(n);
+
+		using U = fp2i<T>::type;
+		constexpr U MASK = fp2i<T>::mask;
+
+		std::mt19937_64 gen(SEED);
+		std::uniform_int_distribution<U> dist(std::numeric_limits<U>::min(), std::numeric_limits<U>::max());
+
+		while (n--)
+		{
+			U num = 0;
+			while (true)
+			{
+				num = dist(gen);
+
+				if (testStrongOrder || (num & MASK) != MASK)
+					break;
+			}
+			v.emplace_back(bit_cast<T>(num));
+		}
+
+		return v;
+	}
+	
+	template<typename T> requires (!std::integral<T> && !std::floating_point<T>)
+	static std::vector<T> generate(int n)
+	{
+		if constexpr (std::same_as<T, std::string>)
+			return generateStrings(n);
 		else
 			static_assert(sizeof(T) == 0, "ERROR: Unable to generate vector!\nCAUSE: Unsupported type!\n");
 	}
